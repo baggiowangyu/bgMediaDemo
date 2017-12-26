52ad5a02b62d79abd63e40a8b0bd89987aca070a
baggiowangyu <baggiowangyu@gmail.com>
2017-12-26 19:10:09 +0800

    增加推流器扩展板视音频预处理逻辑

--- bgMediaDemo/Section_04/Lesson_01/GMMediaPusherEx/GMStreamPusherEx.cpp
+++ bgMediaDemo/Section_04/Lesson_01/GMMediaPusherEx/GMStreamPusherEx.cpp
@@ -31,12 +31,32 @@ int GMStreamPusherEx::OpenTargetUrl(const char *target_url)
 	return errCode;
 }
 
-int GMStreamPusherEx::SetVideoParam(int video_codec, int width, int height, int frame_rate)
+int GMStreamPusherEx::SetVideoParam(int width, int height, int frame_rate, int video_codec /* = MEDIA_CODEC_H264 */, int color_fmt /* = VIDEO_COLOR_FMT_YUV420 */)
 {
 	int errCode = 0;
+	enum AVCodecID av_video_codec_id = AV_CODEC_ID_NONE;
+	enum AVPixelFormat av_pixel_fmt = AV_PIX_FMT_NONE;
+
+	switch (video_codec)
+	{
+	case MEDIA_CODEC_H264:
+		av_video_codec_id = AV_CODEC_ID_H264;
+		break;
+	}
+
+	switch (color_fmt)
+	{
+	case VIDEO_COLOR_FMT_YUV420:
+		av_pixel_fmt = AV_PIX_FMT_YUV420P;
+		break;
+	}
+
+	output_video_codec = avcodec_find_encoder(av_video_codec_id);
+	if (!output_video_codec)
+		return -2;
 
 	// 这里要准备好输出的AVCodecContext
-	output_video_codec_context = avcodec_alloc_context3(AV_CODEC_ID_H264);
+	output_video_codec_context = avcodec_alloc_context3(output_video_codec);
 	if (!output_video_codec_context)
 		return -1;
 
@@ -44,12 +64,89 @@ int GMStreamPusherEx::SetVideoParam(int video_codec, int width, int height, int 
 	output_video_codec_context->height = height;
 	output_video_codec_context->time_base.num = 1;
 	output_video_codec_context->time_base.den = frame_rate;
+	output_video_codec_context->pix_fmt = av_pixel_fmt;
+	output_video_codec_context->bit_rate = 600000;
+	output_video_codec_context->codec_type = AVMEDIA_TYPE_VIDEO;
 
 	return 0;
 }
 
-int GMStreamPusherEx::SetAudioParam(int audio_codec, int sample_rate, int bit, int channels)
+int GMStreamPusherEx::SetAudioParam(int bit_rate, int sample_rate, int sample_fmt /* = AUDIO_BIT_S16 */, int channel_layout /* = AUDIO_CHENNAL_LAYOUT_STEREO */, int audio_codec /* = MEDIA_CODEC_AAC */)
 {
+	int errCode = 0;
+
+	enum AVCodecID av_audio_codec_id = AV_CODEC_ID_NONE;
+	uint64_t audio_channel_layout = AV_CH_LAYOUT_STEREO;
+	AVSampleFormat av_audio_sample_fmt = AV_SAMPLE_FMT_NONE;
+
+	switch (audio_codec)
+	{
+	case MEDIA_CODEC_H264:
+		av_audio_codec_id = AV_CODEC_ID_H264;
+		break;
+	}
+
+	switch (channel_layout)
+	{
+	case AUDIO_CHENNAL_LAYOUT_MONO:
+		audio_channel_layout = AV_CH_LAYOUT_MONO;
+		break;
+	case AUDIO_CHENNAL_LAYOUT_STEREO:
+		audio_channel_layout = AV_CH_LAYOUT_STEREO;
+		break;
+	}
+
+	switch (sample_fmt)
+	{
+	case AUDIO_BIT_U8:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_U8;
+		break;
+	case AUDIO_BIT_S16:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_S16;
+		break;
+	case AUDIO_BIT_S32:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_S32;
+		break;
+	case AUDIO_BIT_FLT:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_FLT;
+		break;
+	case AUDIO_BIT_DBL:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_DBL
+		break;
+	case AUDIO_BIT_U8P:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_U8P;
+		break;
+	case AUDIO_BIT_S16P:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_S16P;
+		break;
+	case AUDIO_BIT_S32P:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_S32P;
+		break;
+	case AUDIO_BIT_FLTP:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_FLTP;
+		break;
+	case AUDIO_BIT_DBLP:
+		av_audio_sample_fmt = AV_SAMPLE_FMT_DBLP;
+		break;
+	}
+
+	output_audio_codec = avcodec_find_encoder(av_audio_codec_id);
+	if (!output_audio_codec)
+		return -2;
+
+	// 这里要准备好输出的AVCodecContext
+	output_audio_codec_context = avcodec_alloc_context3(output_audio_codec);
+	if (!output_audio_codec_context)
+		return -1;
+
+	output_audio_codec_context->sample_rate = sample_rate;
+	output_audio_codec_context->bit_rate = bit_rate
+	output_audio_codec_context->channel_layout = audio_channel_layout;
+	output_audio_codec_context->channels = av_get_channel_layout_nb_channels(output_audio_codec_context->channel_layout);
+	output_audio_codec_context->pix_fmt = av_audio_sample_fmt;
+	output_audio_codec_context->bit_rate = 64000;
+	output_audio_codec_context->codec_type = AVMEDIA_TYPE_AUDIO;
+
 	return 0;
 }
 
--- bgMediaDemo/Section_04/Lesson_01/GMMediaPusherEx/GMStreamPusherEx.h
+++ bgMediaDemo/Section_04/Lesson_01/GMMediaPusherEx/GMStreamPusherEx.h
@@ -1,8 +1,34 @@
 #ifndef _GM_STREAM_PUSHER_EX_H_
 #define _GM_STREAM_PUSHER_EX_H_
 
-#define MEDIA_CODEC_H264
-#define MEDIA_CODEC_AAC
+// ÊÓÆµ±àÂëÆ÷ID
+#define MEDIA_CODEC_H264				0x00000001
+	
+// ÒôÆµ±àÂëÆ÷ID
+#define MEDIA_CODEC_AAC					0x00000001
+#define MEDIA_CODEC_MP3					0x00000002
+#define MEDIA_CODEC_G711u				0x00000003
+
+// Í¼ÏñÑÕÉ«Ä£Ê½
+#define VIDEO_COLOR_FMT_YUV420			0x00000001
+
+// ÒôÆµÉùµÀ·½Ê½
+#define AUDIO_CHENNAL_LAYOUT_MONO		0x00000001
+#define AUDIO_CHENNAL_LAYOUT_STEREO		0x00000002
+
+// ÒôÆµÎ»Êý
+#define AUDIO_BIT_U8					0x00000001
+#define AUDIO_BIT_S16					0x00000002
+#define AUDIO_BIT_S32					0x00000003
+#define AUDIO_BIT_FLT					0x00000004
+#define AUDIO_BIT_DBL					0x00000005
+
+#define AUDIO_BIT_U8P					0x00000006
+#define AUDIO_BIT_S16P					0x00000007
+#define AUDIO_BIT_S32P					0x00000008
+#define AUDIO_BIT_FLTP					0x00000009
+#define AUDIO_BIT_DBLP					0x0000000A
+
 
 class GMStreamPusherEx
 {
@@ -14,14 +40,17 @@ public:
 	int OpenTargetUrl(const char *target_url);
 
 public:
-	int SetVideoParam(int video_codec, int width, int height, int frame_rate);
-	int SetAudioParam(int audio_codec, int sample_rate, int bit, int channels);
+	int SetVideoParam(int width, int height, int frame_rate, int video_codec = MEDIA_CODEC_H264, int color_fmt = VIDEO_COLOR_FMT_YUV420);
+	int SetAudioParam(int bit_rate, int sample_rate, int sample_fmt = AUDIO_BIT_S16, int channel_layout = AUDIO_CHENNAL_LAYOUT_STEREO, int audio_codec = MEDIA_CODEC_AAC);
 	int InputMediaData(const unsigned char *data, int data_len);
 
 private:
 	AVFormatContext *output_format_context;
 	AVOutputFormat *output_format;
 	AVCodecContext *output_video_codec_context;
+	AVCodecContext *output_audio_codec_context;
+	AVCodec *output_video_codec;
+	AVCodec *output_audio_codec;
 };
 
 #endif//_GM_STREAM_PUSHER_EX_H_
